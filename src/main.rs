b4a969fba1b4af59bdfd2ae999753fbbe4fd1f04
tshinohara <takuma1201@gmail.com>
2024-01-20 15:33:09 +0900

    ContentのMedatadaモデルを定義

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use nostr_sdk::prelude::*;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::fs;
 use std::time::Duration;
 // use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
@@ -14,6 +15,37 @@ fn load_config(file_path: &str) -> Result<Config, Box<dyn std::error::Error>> {
 struct Config {
     keys: Vec<String>,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Content {
+    #[serde(flatten)]
+    profile: Profile,
+    #[serde(flatten)]
+    options: HashMap<String, String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Profile {
+    #[serde(default)]
+    name: Option<String>,
+    #[serde(default)]
+    display_name: Option<String>,
+    #[serde(default)]
+    picture: Option<String>,
+    #[serde(default)]
+    about: Option<String>,
+    #[serde(default)]
+    website: Option<String>,
+    #[serde(default)]
+    banner: Option<String>,
+    #[serde(default)]
+    nip05: Option<String>,
+    #[serde(default)]
+    lud06: Option<String>,
+    #[serde(default)]
+    lud16: Option<String>,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let config_path = "./config.json";
@@ -45,17 +77,30 @@ async fn main() -> Result<()> {
             .get_events_of(vec![filters], Some(Duration::from_secs(10)))
             .await?;
         // println!("respnse : {events:#?}")
-        if let Some(first_event) = events.get(0) {
-            let metadata = Metadata::new();
-            println!("{first_event:#?}");
-            // .name(first_event.inner.id)
-            // .display_name("My Username")
-            // .about("Description")
-            // .picture(Url::parse("https://example.com/avatar.png")?)
-            // .banner(Url::parse("https://example.com/banner.png")?)
-            // .nip05("username@example.com")
-            // .lud16("yuki@getalby.com")
-            // .custom_field("custom_field", "my value");
+        if let Some(event) = events.get(0) {
+            // let content = &event.content;
+            // println!("{content:#?}");
+            let deserialized_content: Result<Content, serde_json::Error> =
+                serde_json::from_str(&event.content);
+            let content = match deserialized_content {
+                Ok(c) => c,
+                Err(e) => {
+                    panic!("json解析失敗: {}", e);
+                }
+            };
+            println!("{content:#?}");
+
+            // let metadata = Metadata::new()
+            //     .name("")
+            //     .display_name("My Username")
+            //     .about("Description")
+            //     .picture("")
+            //     .banner("")
+            //     .nip05("username@example.com")
+            //     .lud16("yuki@getalby.com")
+            //     .lud06("")
+            //     .website("");
+            // metadata.custom_field("custom_field", "my value");
         }
 
         // let event: Event = EventBuilder::metadata(&metadata).to_event(&my_keys)?;
